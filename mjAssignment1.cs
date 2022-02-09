a4db3cebe6a8b3c006692acf3be0dca6ddcc6441
Linkedynn <86744320+Linkedynn@users.noreply.github.com>
2022-02-09 15:01:58 -0500

    Add files via upload

---
+++ mjAssignment1.cs
@@ -0,0 +1,11 @@
+﻿//mjAssignment1
+// MJ Ahmed
+// Feb 9, 2022
+// a = 5 sr 3 a2
+
+int Edge = Convert.ToInt32(Console.ReadLine());
+
+double Area = 5.0 * Math.Pow(3, 0.5) * Math.Pow(Edge, 2);
+
+Console.WriteLine(Area);
+Console.ReadLine();
