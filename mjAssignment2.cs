23b51278880deb17b6ea19f0d995ca409f69121c
Linkedynn <86744320+Linkedynn@users.noreply.github.com>
2022-02-10 16:05:29 -0500

    Add files via upload

---
+++ mjAssignment2.cs
@@ -0,0 +1,9 @@
+﻿//mjAssignment2 
+// MJ Ahmed
+// Feb 10, 2022
+
+for (int i = 5283; i < 5307 ; i++)
+{
+     Console.WriteLine(Convert.ToChar(i));
+}
+// Code only displays ascii symbols if u run it in vs code terminal, Not external (some still show as ?)
